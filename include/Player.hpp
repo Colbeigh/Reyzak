42f1184facc78d52e7b0159eabdf8d8cdd14574d
Jem <j.pineda@uleth.ca>
2024-09-24 17:54:55 -0600

    Adding Player and PlayerWallet hpp file

---
+++ include/Player.hpp
@@ -0,0 +1,14 @@
+// Minimal source file for a placebo class that does nothing but allows the
+// pipeline to pass.
+
+#ifndef PLAYER_HPP_INCLUDED
+#define PLAYER_HPP_INCLUDED
+
+class Player {
+ public:
+  
+  Player();
+  virtual ~PlaceboClass();
+};
+
+#endif //PLAYER_HPP_INCLUDED
