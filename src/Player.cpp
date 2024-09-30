f0266b83945798ed4c99b45f3f706e86c5300470
Jem <j.pineda@uleth.ca>
2024-09-30 15:02:27 -0600

    updating player constructor

--- src/Player.cpp
+++ src/Player.cpp
@@ -3,19 +3,32 @@ Author: Jem Pineda
 Dates:
 
  */
+/**
+want:
+target in bool and player
+current bet public
+const static_cast from int to double
 
+
+ */
+
+#include "PWallet.hpp"
 #include "Player.hpp"
 
-//Parameters, player score and current Score
-Player::Player(int pScore, int currentScore){
+//Parameters, Player 
+Player::Player(int Pscore, int currentBet, PWallet Wallet){
+Pscore = 0;
+
+    if(Pcore == Ptarget);
 
 }
 
 //Parameters, interger amount for bet
-Player:: Placebet(int amount){
-amount = 0;
+Player:: placeBet(double currentBet){
+currentBet = 0;
+
 
 std::cout << "Place your bet:"
 std::cin >> amount;
-
+ 
 }
