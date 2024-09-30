f475094f5db4431b53375e997362aca9e1487bbd
hanc2720 <colby.hanna@uleth.ca>
2024-09-30 12:28:21 -0600

    Fixing style check errors

--- src/Game.cpp
+++ src/Game.cpp
@@ -6,22 +6,21 @@
 
 #include "Player.hpp"
 #include "Spinner.hpp"
-#include "Player.hpp"
+
 #include "Payout.hpp"
 
 #include <iostream>
 #include <cctype>
 
-Game::Game() {} // I believe unneccesary in this class to further implement just going to use default
+Game::Game() {} // I believe unneccesary in //this class to further implement just going to use default
+Game::~Game() {} // I believe unneccesary in //this class to further implement just going to use default
 
-Game::~Game() {} // I believe unneccesary in this class to further implement just going to use default
-
-  void Start(){
-    while (zephy > 0 || playing == true){ // change to player.wallet.zephy
+  void Start() {
+    while (zephy > 0 || playing == true) { // change to player.wallet.zephy
         player.PlaceBet();
         Stakes(bet);
         SelectSpinner();
-            while (player.bust() == false || gambling == true){
+            while (player.bust() == false || gambling == true) {
                 hit();
             }
             payout.Payout(); // calls payout function can change to whatever we decide to call it or we can implement into however we want
@@ -30,24 +29,20 @@ Game::~Game() {} // I believe unneccesary in this class to further implement jus
     Exit();
   }
 
-  void Stakes(int bet){
-    if (bet < 25){ // Change to Player.bet once implemented
+  void Stakes(int bet) {
+    if (bet < 25) { // Change to Player.bet once implemented
         stakes = "LowStakes";
-    }
-    else if (bet >= 25 && bet < 50){ // Change to Player.bet once implemented
+    } else if (bet >= 25 && bet < 50) { // Change to Player.bet once implemented
         stakes = "MediumStakes";
-    }
-    else{
+    } else {
         stakes = "HighStakes";
-
     }
-    
   }
 
-  void SelectSpinner(){
+  void SelectSpinner() {
     char input;
 
-    while(true) {
+    while (true) {
         std::cout << "Please Select a Spinner" << std::endl << "A - 2 to 5 Spinner" << std::endl << "B - 0 to 7 Spinner" << std::endl;
         std::cin >> input;
 
@@ -57,63 +52,55 @@ Game::~Game() {} // I believe unneccesary in this class to further implement jus
             spinner = "2 to 5"; // update to spinner class once implemented
             std::cout << "You have selected the 2 to 5 spinner!" << std::endl;
             break;
-        }
-        else if (input == 'B') {
+        } else if (input == 'B') {
             spinner ="0 to 7"; // update to spinner class once implemented
             std::cout << "You have selected the 0 to 7 spinner!" << std::endl;
             break;
-        }
-        else {
+        } else {
             std::cout<< "That is an invalid input. Please try Again" << std::endl;
-        }   
+        }
     }
   }
 
-
-
-  void hit(){
+  void hit() {
     char input2;
-    
+
     std::cout<< "Your current score is: " << score << std::endl; // Update to player.score once implemented
-    
-    while(true){
+
+    while (true) {
         std::cout << "Would you like to spin?  Y/N" << std::endl;
         std::cin >> input2;
 
         input2 = std::toupper(input2);
-        if (input2 == 'Y'){
+        if (input2 == 'Y') {
             int rndmnum = spin(stakes, spinner);  // change to how we call spin
             std::cout << "You spun " << rndmnum << "!" << std::endl;
 
             score += rndmnum; // Update to player.score once implemented
             std::cout << "Your new score is " << score << std::endl; // Update to player.score once implemented
             break;
-        }
-        else if (input2 == 'N'){
+        } else if (input2 == 'N') {
             gambling = false;
             std::cout << "You decided not to spin" << std::endl;
-        }
-        else{
+        } else {
             std::cout<< "Invalid input" << std::endl;
         }
     }
   }
 
-  void Exit(){
+  void Exit() {
     std::cout << "Thank's for playing!" << std::endl;
     std::cout << "You left the table with " << zephy << " Zephy" << std::endl; // replace with Player.zephy
 
-    if (zephy <= 0){
+    if (zephy <= 0) {
         std::cout << "Better luck next time" << std::endl;
-    }
-    else if (zephy > 50){ // replace with Player.zephy
+    } else if (zephy > 50){ // replace with Player.zephy
         std::cout << "You made a " << zephy - 50 << " profit!" << std::endl; // replace with Player.zephy
     }
     playing = false;
   }
 
-  void Payout(){ // We might not use so have not implemented
-
+  void Payout() { // We might not use so have not implemented
 
   }
 
@@ -124,6 +111,5 @@ Game::~Game() {} // I believe unneccesary in this class to further implement jus
     int score = 0; // will be deleted
     int current_score = 0; // will be deleted
     int zephy = 50; // will be deleted
-
     bool playing = true; // used for loop in Start()
     bool gambling = true;// used for loop in Start()
