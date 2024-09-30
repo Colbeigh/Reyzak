69fe4976d15e20ca7dfd858a16ce5ad1078d0439
pinj2720 <j.pineda@uleth.ca>
2024-09-30 12:44:50 -0600

    Adding Player.cpp

---
+++ src/Player.cpp
@@ -0,0 +1,21 @@
+/**
+Author: Jem Pineda
+Dates:
+
+ */
+
+#include "Player.hpp"
+
+//Parameters, player score and current Score
+Player::Player(int pScore, int currentScore){
+
+}
+
+//Parameters, interger amount for bet
+Player:: Placebet(int amount){
+amount = 0;
+
+std::cout << "Place your bet:"
+std::cin >> amount;
+
+}
