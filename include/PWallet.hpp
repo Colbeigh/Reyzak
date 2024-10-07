bea4659382d6b828ebff0036edeac6d03790f18b
pinj2720 <j.pineda@uleth.ca>
2024-10-06 22:34:57 -0600

    Finished doxygen for PlayerWallet.hpp

--- include/PWallet.hpp
+++ include/PWallet.hpp
@@ -4,15 +4,29 @@
 */
 #ifndef PWALLET_HPP_INCLUDED
 #define PWALLET_HPP_INCLUDED
-
+/**
+  * @class PWallet PWallet.cpp "PWallet.hpp"
+  * @brief Handles the currency name Zephy
+  */
 class PWallet {
  public:
+ /**
+  * @brief A Default Constructor that initializes the currency
+  */
 PWallet();
-~PWallet();
+/**
+  * @brief Checks the current balance
+  * @return the current balance
+  */
 double checkBalance() const;
+/**
+  * @brief Removes appropriate amount of Zephy 
+  */
 void removeZephy(double amount);
+/**
+  * @brief Adds appropriate amount of Zephy
+  */
 void addZephy(double amount);
-
 
  private:
 double zephy;
