7f0154c9fefd91144c849a2115994edeac6a2ee4
Todd <t.acrossthemountain@uleth.ca>
2024-10-10 08:44:39 -0600

    Style check

--- include/2to5_Spinner.hpp
+++ include/2to5_Spinner.hpp
@@ -9,15 +9,15 @@
  * @brief An inherited class that applies polymorphism on spin functionality depending on players stakes within the range 2-5
  */
 class FiveSpinner : public Spinner {
-  public:
-    //override function for spin
-    int spin(std::string Stakes) override;
+ public:
+  //override function for spin
+  int spin(std::string Stakes) override;
 
-    //High Stakes function, has bias for higher numbers
-    int HighStakes(int NumberGuessed) override;
+  //High Stakes function, has bias for higher numbers
+  int HighStakes(int NumberGuessed) override;
 
-    //Low Stakes function, has bias for lower numbers
-    int LowStakes(int NumberGuessed) override;
+  //Low Stakes function, has bias for lower numbers
+  int LowStakes(int NumberGuessed) override;
 };
 
 #endif
