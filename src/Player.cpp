8434a3cb3a29b0d41fd384606ab958f435abd215
hanc2720 <colby.hanna@uleth.ca>
2024-10-09 22:19:42 -0600

    Moved your new balance in getPayout() out of if statement so it'll print even if you lose

--- src/Player.cpp
+++ src/Player.cpp
@@ -52,6 +52,6 @@ void Player::getPayout() {
     currentbet = Pay.calculatePayout(currentscore, currentbet);
     if (currentbet > 0) {
         Wallet.addZephy(currentbet);
-        std::cout << "Your new balance is $" << Wallet.checkBalance() << "/n";
     }
+    std::cout << "Your new balance is $" << Wallet.checkBalance() << "\n";
 }
