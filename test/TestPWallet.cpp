b26e3ac688452dcc5f46b6804bfa5015d08d7d30
pinj2720 <j.pineda@uleth.ca>
2024-10-10 19:29:32 -0600

    "Testing PWalletTest"

--- test/TestPWallet.cpp
+++ test/TestPWallet.cpp
@@ -2,3 +2,27 @@
  * @author Jem Pineda <j.pineda@uleth.ca>
  * @date Fall 2024
 */
+#include "gtest/gtest.h"
+#include "PWallet.hpp"
+
+TEST(TestPWallet, defaultConstructorTest) {
+    PWallet Wallet;
+    EXPECT_EQ(Wallet.zephy, 50);
+}
+
+TEST(TestPWallet, checkBalanceTest) {
+    PWallet Wallet;
+    EXPECT_EQ(Wallet.checkBalance(), 50);
+}
+
+TEST(TestPWallet, addZephyTest) {
+    PWallet Wallet;
+    EXPECT_EQ(Wallet.addZephy(0), 50);
+    EXPECT_EQ(Wallet.addZephy(10), 60);
+}
+
+TEST(TestPWallet, removeZephyTest) {
+    PWAllet Wallet;
+    EXPECT_EQ(Wallet.removeZephy(0), 50);
+    EXPECT_EQ(Wallet.removeZephy(10), 40);
+}
